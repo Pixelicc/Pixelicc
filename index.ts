0b7da3add807b84bbeabf3fe0901596343f37d67
Pixelic <69585766+Pixelicc@users.noreply.github.com>
2024-05-21 10:28:11 +0200

    Fixed Sunny Weather appearing during the Night

--- index.ts
+++ index.ts
@@ -7,11 +7,10 @@ const template = Handlebars.compile(await Bun.file("README.template").text());
 const emojis = ["😊", "😄", "😇", "👀"];
 const choosenEmoji = emojis[Math.floor(Math.random() * emojis.length)];
 
-const weather = (await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${process.env.WEATHER_LATITUDE}&longitude=${process.env.WEATHER_LONGITUDE}&current=temperature,rain,snowfall,cloud_cover&hourly=temperature,rain,snowfall&daily=sunrise,sunset&timezone=GMT&forecast_days=1`)).data;
-const weatherHuman = `${weather.current.temperature} °C and ${weather.current.rain ? "rainy 🌧️" : weather.current.snowfall ? "snowy 🌨️" : weather.current.cloud_cover > 95 ? "cloudy ☁️" : weather.current.cloud_cover > 85 ? "partially cloudy 🌥️" : weather.current.cloud_cover > 75 ? "partially cloudy ⛅" : "sunny ☀️"}`;
-
 const localTime = moment().tz(process.env.TIMEZONE as string);
 const localClock = localTime.format("HH:mm");
+const weather = (await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${process.env.WEATHER_LATITUDE}&longitude=${process.env.WEATHER_LONGITUDE}&current=temperature,rain,snowfall,cloud_cover&hourly=temperature,rain,snowfall&daily=sunrise,sunset&timezone=GMT&forecast_days=1`)).data;
+const weatherHuman = `${weather.current.temperature} °C and ${weather.current.rain ? "rainy 🌧️" : weather.current.snowfall ? "snowy 🌨️" : weather.current.cloud_cover > 95 ? "cloudy ☁️" : weather.current.cloud_cover > 85 ? "partially cloudy 🌥️" : weather.current.cloud_cover > 75 ? "partially cloudy ⛅" : localTime.hour() > moment(weather.daily.sunset[0]).hour() ? "clear" : "sunny ☀️"}`;
 const localTimeHuman = localTime.hour() < 23 && localTime.hour() > moment(weather.daily.sunset[0]).hour() - 1 ? `${localClock} in the evening` : localTime.hour() > 12 ? `${localClock} in the afternoon` : localTime.hour() > moment(weather.daily.sunrise[0]).hour() ? `${localClock} in the morning` : `${localClock} during the night`;
 
 const codeStats = (await axios.get(process.env.WAKATIME_JSON_EMBED_URL as string)).data;
